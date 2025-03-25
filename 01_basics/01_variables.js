15124b97b8977ead39c4c07abf85959cec90dec8
Vandana-singh-307 <vandanasingh4534@gmail.com>
2025-03-25 08:05:55 +0000

    var class

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+// Variables :- Variables are used to store some information in memory. 
+
+// Try to learn things in an Investigation manner.
+const accountId = 144553
+let accountEmail = "vandanasingh4534@gmail.com"
+var accountPassword = "12345"
+accountCity = "Orai"
+let accountState;
+
+// accountId = "2"; // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword  = " 212121"
+accountCity = "Bengaluru"
+
+/*
+
+Prefer Not to use var
+because of the issue in block and function scope.
+
+If you define variable without assigning the value,
+By default it define as undefined.
+
+*/ 
+
+console.table([accountEmail,accountPassword,accountCity,accountState
+
+]);
