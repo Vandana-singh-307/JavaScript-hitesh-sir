fa7cfb8df896873a517a76f78b5d435c9812c93c
Vandana-singh-307 <vandanasingh4534@gmail.com>
2025-04-01 19:27:18 +0000

    datatype overview

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS codde as newer version
+/*
+JavaScript engine lies in Browser
+
+
+
+*/
+
+// alert("Hello"); We're using nodejs not browser 
+//Primitive Datatypes in JS
+//Number => 2^52
+//bigInt => 
+//boolean
+//string
+//null => a standalone value
+//undefined
+//symbol => used for Uniqueness
+
+
+//Objects
+
+let age = 55;
+console.log(typeof(age))
+
+console.log(typeof null);//object
+console.log(typeof undefined) //undefined
